ad77d032007602b333017778630c80ffffbfd321
xiaqinglin <xia.qinglin@frontsurf.com>
2022-01-01 23:44:59 +0800

    create api with mongodb(insert, query)

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
+import json
+
+from models import Guild
+from models import Nft
+from models import Members
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -10,13 +15,87 @@ app.config['MONGODB_SETTINGS'] = {
 }
 db = MongoEngine()
 db.init_app(app)
-api = Api(app, version='1.0', title='Sample API',
-          description='A sample API')
+api = Api(app, version='1.0', title='Rostra Backend API',
+          description='Rostra Backend Restful API')
+rostra_conf = api.namespace('Rostra', description='Rostra APIs')
 
 
-@api.route('/get-guilds/<address>', methods=['GET'])
-@api.doc(params={'address': 'wallet address'})
-class get(Resource):
+@rostra_conf.route('/get-guilds/', methods=['GET'])
+@api.response(200, 'Query Successful')
+class Get(Resource):
     def get(self):
-        return {'get': 'guild'}
+        guilds = Guild.objects()
+        return {"result": guilds.to_json()}
+
+
+@rostra_conf.route('/get-guilds/<address>', methods=['GET'])
+@rostra_conf.doc(params={'address': 'wallet address'})
+@api.response(200, 'Query Successful')
+class Get(Resource):
+    def get(self, address):
+        print(address)
+        query_by_address = Guild.objects(wallet_address=address)
+        return {"result": query_by_address.to_json()}
+
+
+nft = rostra_conf.model('Nft', {
+    'name': fields.String,
+    'baseURI': fields.String
+})
+
+members = rostra_conf.model('members', {
+    'nfts': fields.List(fields.Nested(nft)),
+    'guilds': fields.List(fields.Integer)
+})
+
+resource_fields = rostra_conf.model('guild', {
+    "guild_id": fields.Integer(required=True, description='The guild id identifier'),
+    'name': fields.String(required=True, description='The guild name identifier'),
+    "desc": fields.String,
+    "creator": fields.String,
+    "wallet_address": fields.String(required=True, description='The user wallet address'),
+    "signature": fields.String,
+    "members": fields.Nested(members)
+})
+
+
+@rostra_conf.route('/add-guild/', methods=['POST'])
+class Add(Resource):
+    @rostra_conf.doc(body=resource_fields, responses={201: 'Guild Created'})
+    @api.response(500, 'Internal Error')
+    def post(self):
+        try:
+            data = api.payload
+            guild_id = data['guild_id']
+            name = data['name']
+            desc = data['desc']
+            creator = data['creator']
+            wallet_address = data['wallet_address']
+            signature = data['signature']
+            nfts = data['members']['nfts']
+            guilds = data['members']['guilds']
+            guilds_array = []
+            for guild in guilds:
+                guilds_array.append(int(guild))
+            print(guilds_array)
+            nft_array = []
+            for nft in nfts:
+                nft_name = nft['name']
+                nft_baseURI = nft['baseURI']
+                nft_obj = Nft(name=nft_name, baseURI=nft_baseURI)
+                nft_array.append(nft_obj)
+            print(nft_array)
+            members = Members(nfts=nft_array, guilds=guilds_array)
+            guild = Guild(name=name,
+                          guild_id=guild_id,
+                          desc=desc,
+                          creator=creator,
+                          wallet_address=wallet_address,
+                          signature=signature,
+                          members=members)
+
+            guild.save()
+            return json.dumps({'message': 'SUCCESS'})
+        except Exception as e:
+            return json.dumps({'error': str(e)})
 
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-from mongoengine import Document
+import mongoengine as me
 from mongoengine import (
     IntField,
     StringField,
-    ReferenceField,
     ListField,
+    EmbeddedDocumentField
 )
 
 
-class NFT:
+class Nft(me.EmbeddedDocument):
     name = StringField()
     baseURI = StringField()
 
@@ -15,24 +15,26 @@ class NFT:
         return "%s : %s\n" % (self.name, self.baseURI)
 
 
-class Members(Document):
-    nfts = ListField(ReferenceField(NFT))
-    guilds = ListField(IntField)
+class Members(me.EmbeddedDocument):
+    nfts = ListField(EmbeddedDocumentField('Nft'))
+    guilds = ListField(IntField())
 
     def __repr__(self):
         return "%s : %s\n" % (str(self.nfts), str(self.guilds))
 
 
-class Guild(Document):
+class Guild(me.Document):
+    guild_id = IntField()
     name = StringField()
     email = StringField()
     desc = StringField()
     creator = StringField()
+    wallet_address = StringField()
     signature = StringField()
-    members = ReferenceField(Members)
+    members = EmbeddedDocumentField('Members')
 
     def __repr__(self):
-        return "%s : %s : %s : %s : %s : %s \n" % self.name, self.email, self.desc, self.creator, self.signature, self.members
+        return "%s : %s : %s : %s : %s : %s : %s \n" % self.guild_id, self.name, self.email, self.desc, self.creator, self.signature, self.members
 
 
 
