58ed34a2a6fb6311127e06bb63f9e95c5958f7f1
xiaqinglin <xia.qinglin@frontsurf.com>
2022-02-16 08:51:51 +0800

    bug fix

--- models.py
+++ models.py
@@ -28,6 +28,9 @@ class Guild(me.Document):
     name = StringField()
     desc = StringField()
     creator = StringField()
+    members = ListField(StringField())
+    signature = StringField()
+    requirements = EmbeddedDocumentField('Requirements')
 
     def __repr__(self):
         return "%s , %s , %s , %s\n " % (self.guild_id, self.name, self.desc, self.creator)
